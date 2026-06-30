80d58ea0a2986cd2f28cafe7e04605a5844384d7
Ivan <tokarevv345@gmail.com>
2026-07-01 00:45:12 +0300

    добавлен полезный скрипт

---
+++ git test/tester.py
@@ -0,0 +1,15 @@
+print("напиши пук пук: ")
+puk_puk = input()
+def pukpuk():
+    print("какакакакака пук пук")
+if puk_puk == "пук пук":
+    pukpuk()
+    pukpuk()
+    pukpuk()
+    pukpuk()
+else:
+    print("друг: зовет меня гулять")
+    print("так же я, который кушаю кабачки:")
+    print("ора ора ора ора ора орал")
+
+
